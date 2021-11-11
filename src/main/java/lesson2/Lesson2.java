c6b5dda094cfaae28680506dd477c602dc407c08
ivan8899 <f7jBW["lWPS3>
2021-11-11 17:03:53 +0700

    Lesson2: Простые арифметические операции и вызов методов

--- src/main/java/lesson2/Lesson2.java
+++ src/main/java/lesson2/Lesson2.java
@@ -6,19 +6,32 @@ public class Lesson2 {
         System.out.println("Привет мир123!");
 
         int fistSumma = calculate(20, -40);
-        System.out.println(fistSumma);
+        System.out.println("Сумма чисел 20 и -40 равна " + fistSumma);
+
         int otherSumma = calculate(5, 8);
-        System.out.println(otherSumma);
+        System.out.println("Сумма чисел 5 и 8 равна " + otherSumma);
 
         int secondSumma = summa(5, 8, 6);
-        System.out.println(secondSumma);
+        System.out.println("Сумма выражения (5 - 8) * 6 равна " + secondSumma);
 
         int thirdSumma = multiplyAndSumma(5);
-        System.out.println(thirdSumma);
+        System.out.println("Сумма метода multiplyAndSumma равна " + thirdSumma);
 
         int volume = roomVolume(5, 4, 3);
-        System.out.println(volume);
+        System.out.println("Объем квартиры в метрах кубических равен " + volume);
 
+        double div = division(35, 3);
+        System.out.println("Сумма деления 35 на 3 равна " + div);
+
+        int operand = 42;
+        double div1 = division1(operand, 4);
+        System.out.println("Сумма деления " + operand + " на 4 равна " + div1);
+
+        int result = pow(3, 5);
+        System.out.println("Возведение в степень чилса 3 в степень 5 = " + result);
+
+        int powrun = mod(21,5);
+        System.out.println("Остаток на деления числа 21 на 5 равен " + powrun);
     }
 
     public static int summa(int arg1, int arg2, int arg3) {
@@ -27,21 +40,39 @@ public class Lesson2 {
 
     /**
      * Метод будет складывать два числа и возвращать результат
-     * @param arg1  первый аргумент
-     * @param arg2  второй аргумент
-     * @return  результат чисел
+     *
+     * @param arg1 первый аргумент
+     * @param arg2 второй аргумент
+     * @return результат чисел
      */
     public static int calculate(int arg1, int arg2) {
         return arg1 + arg2;
-
     }
 
     public static int multiplyAndSumma(int arg1) {
         int result = 44 * arg1 + 28;
-        result = result -150;
+        result = result - 150;
         return result;
     }
+
     public static int roomVolume(int length, int width, int height) {
         return length * width * height;
     }
+
+    public static double division(int a, int b) {
+        return (double) a / b;
+    }
+
+    public static double division1(double a, double b) {
+        return a / b;
+    }
+
+    public static int mod(int i, int i1) {
+        return i % i1;
+    }
+
+    public static int pow(int i, int i1) {
+        return (int) Math.pow(i, i1);
+    }
+
 }
