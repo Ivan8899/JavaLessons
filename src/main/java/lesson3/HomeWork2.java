4ca033e50901bd99e2e2786c6f26c6aadea505bd
ivan8899 <f7jBW["lWPS3>
2021-11-12 20:09:35 +0700

    Lesson3: Вычисление корней квадратного уравнения (Дискриминант) и факториал числа n!

---
+++ src/main/java/lesson3/HomeWork2.java
@@ -0,0 +1,28 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Введите число: ");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        System.out.println("Факториал числа " + n + " равен " + result);
+    }
+
+
+}
+
+
+
+
+
+
+
+
